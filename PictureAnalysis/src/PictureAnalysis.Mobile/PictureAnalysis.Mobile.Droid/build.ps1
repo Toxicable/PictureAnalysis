159bf4667198aa6dac76f0fea4c2be3aa0f497c1
Fabian <Fabian@DESKTOP-0MBDLK1>
2016-08-06 21:13:42 +1200

    Added build script

---
+++ PictureAnalysis/src/PictureAnalysis.Mobile/PictureAnalysis.Mobile.Droid/build.ps1
@@ -0,0 +1,7 @@
+﻿# First clean the Release target.
+msbuild.exe PictureAnalysis.Mobile.Droid.csproj /p:Configuration=Release /t:Clean
+
+# Now build the project, using the Release target.
+msbuild.exe PictureAnalysis.Mobile.Droid.csproj /p:Configuration=Release /t:PackageForAndroid
+
+& 'C:\Program Files (x86)\Android\android-sdk\build-tools\21.1.2\zipalign.exe' -f -v 4 C:\Users\Fabian\Dropbox\PictureAnalysis\PictureAnalysis\src\PictureAnalysis.Mobile\PictureAnalysis.Mobile.Droid\bin\Release\TagSnap.TagSnap-Signed.apk C:\Users\Fabian\Dropbox\PictureAnalysis\PictureAnalysis\src\PictureAnalysis.Mobile\PictureAnalysis.Mobile.Droid\bin\Release\com.fabian.tagsnap.apk
